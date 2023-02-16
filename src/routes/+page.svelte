a28c15ef99a6e9a4431ad77741e0cc5d3753a5ad
Christopher Sterza <ChristopherSterza@gmail.com>
2023-02-15 18:53:45 -0800

    feat: added a loading screen
    Added a loading screen to mask pop-in. Especially for the background sketch.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <script>
   import BackgroundComponent from '../lib/components/BackgroundComponent.svelte';
+  import LoadingScreen from '../lib/components/LoadingScreen.svelte';
   import HomeSection from '../lib/sections/HomeSection.svelte';
+
+  let isLoading = true;
 </script>
 
 <svelte:head>
   <title>Hello, I'm Chris Sterza</title>
 </svelte:head>
 
-<main>
-  <BackgroundComponent />
+<!-- TODO: Create minimum loading time so screen doesn't flash the loading page -->
+{#if isLoading}
+  <LoadingScreen />
+{/if}
+<main class:hidden={isLoading}>
+  <BackgroundComponent bind:isLoading />
   <HomeSection />
+  <div class="h-screen" />
 </main>
