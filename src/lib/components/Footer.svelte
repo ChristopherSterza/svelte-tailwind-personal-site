fe0e58adacec5de3a4aa4560046efb4b75d6e8fb
Christopher Sterza <ChristopherSterza@gmail.com>
2023-02-20 18:01:32 -0800

    feat: added footer and spacing
    Added a footer to the end of the page and also added some spacing between sections.

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script></script>
+
+<footer class="container flex flex-col mx-auto mb-12 text-white">
+  <div class="pt-10 mx-4 mb-6 sm:border-l-4 sm:border-lime-400">
+    <!-- Dot -->
+    <span
+      class="hidden relative w-[20px] h-[20px] bg-lime-400 rounded-full -left-3 shrink-0 sm:block"
+    />
+  </div>
+  <div class="flex flex-col items-center mx-4 text-gray-300/50">
+    <p>
+      Made with <span class="text-lime-400">❤</span> by Christopher Sterza
+    </p>
+  </div>
+</footer>
